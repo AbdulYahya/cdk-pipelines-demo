0eeb829225867531dc9ade743b770ec460e7f240
Abdul <api@proofratings.com>
2021-01-15 04:36:20 -0800

    Move to CloudWatch Synthetics testing

--- pipelines_webinar/lambda/handler.ts
+++ pipelines_webinar/lambda/handler.ts
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 export const handler = async (event: any = {}): Promise<any> => {
     return {
-        body: 'Broken',
+        body: 'OK',
         statusCode: 500,
     };
 };

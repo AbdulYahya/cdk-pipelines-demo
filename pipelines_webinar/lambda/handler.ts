34e265f4700ec3b29f1962285d955746747c40da
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2021-01-15 13:58:46 -0800

    Fix branch

--- pipelines_webinar/lambda/handler.ts
+++ pipelines_webinar/lambda/handler.ts
@@ -3,6 +3,6 @@ const AWS = require('aws-sdk');
 export const handler = async (event: any = {}): Promise<any> => {
     return {
         body: 'OK',
-        statusCode: 500,
+        statusCode: 200,
     };
 };
